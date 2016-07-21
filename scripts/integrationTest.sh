2abdd206f3c12b6953a0048f8d603942cc47bbc0
kiddynamo <njenan@gmail.com>
2016-07-20 18:56:42 -0700

    Revert "Adding unirest http client, integration tests."
    This reverts commit 7d4450408afdce6b1d638bc5c2add6c1760ff23f.

--- scripts/integrationTest.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-npm run pre-integration-test
-
-mocha "src/**/__test__/IntegrationTest*.js"
-
-mb stop
--- scripts/startMountebank.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-nohup mb start &
-
-while [ ! -f ./mb.pid ]
-do
-  sleep 1
-done
