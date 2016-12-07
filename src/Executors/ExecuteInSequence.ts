c136727b07a5be64d44a8505233817a310c76c43
kiddynamo <njenan@gmail.com>
2016-12-06 21:10:44 -0800

    Cleaning up logic.

--- src/Executors/ExecuteInSequence.ts
+++ src/Executors/ExecuteInSequence.ts
@@ -1,20 +1,12 @@
 import Q = require('q');
 
 export function ExecuteInSequence<T>(executables: (() => Q.Promise<T>)[]): Q.Promise<T[]> {
-    let allResults: T[] = <T[]> [];
-
-    return executables.reduce(
-        (promise: Q.Promise<T>, nextTestPlan: () => Q.Promise<T>): Q.Promise<T> => {
-            return promise.then((): Q.Promise<T> => {
-                let result = nextTestPlan();
-
-                return result.then((results) => {
-                    allResults.push(results);
-                    return results;
-                });
+    return executables.reduce((promise, executable) => {
+        return promise.then((results) => {
+            return executable().then((result) => {
+                results.push(result);
+                return results;
             });
-        }, Q.resolve(null))
-        .then(() => {
-            return allResults;
         });
+    }, Q.resolve([]));
 }
