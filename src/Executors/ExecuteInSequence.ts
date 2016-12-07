bcb89ed07450010d4540659c8657fb02435f7021
kiddynamo <njenan@gmail.com>
2016-12-06 20:55:47 -0800

    Changing specific type to generic

--- src/Executors/ExecuteInSequence.ts
+++ src/Executors/ExecuteInSequence.ts
@@ -1,11 +1,10 @@
 import Q = require('q');
-import {SummaryResults} from '../Results/SummaryResults';
 
 export function ExecuteInSequence<T>(executables: (() => Q.Promise<T>)[]): Q.Promise<T[]> {
     let allResults: T[] = <T[]> [];
 
     return executables.reduce(
-        (promise: Q.Promise<SummaryResults>, nextTestPlan: () => Q.Promise<T>): Q.Promise<T> => {
+        (promise: Q.Promise<T>, nextTestPlan: () => Q.Promise<T>): Q.Promise<T> => {
             return promise.then((): Q.Promise<T> => {
                 let result = nextTestPlan();
 
